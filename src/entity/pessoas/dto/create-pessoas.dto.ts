af373f99c0d1b491d607abc192fa4f6179ad5fcc
gassenato <gabrielassenato@gmail.com>
2025-07-31 16:48:05 -0300

    feat: update DTOs for Pessoas to include validation rules for nome and email fields

--- src/entity/pessoas/dto/create-pessoas.dto.ts
+++ src/entity/pessoas/dto/create-pessoas.dto.ts
@@ -1,4 +1,23 @@
-export class CreatePessoasDto {
-    readonly nome: string; 
-    readonly email: string;
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsEmail,
+} from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  readonly nome: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly passwordHash: string;
 }
--- src/entity/pessoas/dto/update-pessoas.dto.ts
+++ src/entity/pessoas/dto/update-pessoas.dto.ts
@@ -1,4 +1,13 @@
-export class UpdatePessoasDto {
-    nome?: string; 
-    email?: string;
+import { IsString, IsNotEmpty, MinLength, MaxLength, IsEmail } from "class-validator";
+
+export class UpdatePessoaDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  readonly nome?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email?: string;
 }
