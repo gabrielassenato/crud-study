21c63bde722cec9909b304e51a6d332fe8603ed9
gassenato <gabrielassenato@gmail.com>
2025-07-31 16:14:45 -0300

    feat: enhance PessoasService with complete CRUD functionality and error handling

--- src/entity/pessoas/pessoas.service.ts
+++ src/entity/pessoas/pessoas.service.ts
@@ -1,27 +1,75 @@
-import { Injectable } from "@nestjs/common";
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Pessoa } from './entities/pessoa.entity';
 
 @Injectable()
 export class PessoasService {
-    findAll() {
-        return "Lista de pessoasss";
+  private lastId = 1; // Simula um ID auto-incremental
+  private pessoas: Pessoa[] = [
+    {
+      id: 1,
+      nome: 'fulano',
+      email: 'fulano@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
+
+  findAll() {
+    return this.pessoas;
+  }
+
+  findOne(id: string) {
+    const pessoa = this.pessoas.find((pessoa) => pessoa.id === +id);
+
+    if (pessoa) return pessoa;
+
+    throw new NotFoundException(`Pessoa com id ${id} não encontrada`);
+  }
+
+  create(body: any) {
+    this.lastId++;
+    const id = this.lastId;
+    const newPessoa: Pessoa = {
+      id,
+      ...body,
+    };
+    this.pessoas.push(newPessoa);
+    return newPessoa;
+  }
+
+  update(id: string, body: any) {
+    const pessoaIndex = this.pessoas.findIndex((pessoa) => pessoa.id === +id);
+
+    if (pessoaIndex < 0) {
+      throw new NotFoundException(`Pessoa com id ${id} não encontrada`);
     }
 
-    findOne(id: string) {
-        return `Detalhes da pessoa com ID: ${id}`;
+    const pessoaExistente = this.pessoas[pessoaIndex];
+
+    this.pessoas[pessoaIndex] = {
+      ...pessoaExistente,
+      ...body,
+    };
+    
+    return this.pessoas[pessoaIndex];
+  }
+
+  remove(id: string) {
+    const pessoaIndex = this.pessoas.findIndex((pessoa) => pessoa.id === +id);
+
+    if (pessoaIndex < 0) {
+      throw new NotFoundException(`Pessoa com id ${id} não encontrada`);
     }
 
-    create(body: any) {
-        return body;
-    }
+    const pessoa = this.pessoas[pessoaIndex];
+    
+    this.pessoas.splice(pessoaIndex, 1);
 
-    update(id: string, body: any) {
-        return {
-            id,
-            ...body // desempacote esse objeto dentro do objeto retornado
-        };
-    }
-
-    remove(id: string) {
-        return `Pessoa com ID: ${id} foi deletada`;
-    }
+    return pessoa;
+  }
 }
