24ca8e1f68fd9a003faa3cd004a4b04c72c4a350
gassenato <gabrielassenato@gmail.com>
2025-09-18 08:59:35 -0300

    feat: add multer types and implement picture upload functionality in PessoasController

--- src/entity/pessoas/pessoas.controller.ts
+++ src/entity/pessoas/pessoas.controller.ts
@@ -1,50 +1,91 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Req, UseGuards } from "@nestjs/common";
-import { PessoasService } from "./pessoas.service";
-import { CreatePessoaDto } from "./dto/create-pessoas.dto";
-import { UpdatePessoaDto } from "./dto/update-pessoas.dto";
-import { AuthTokenGuard } from "src/auth/guards/auth-token.guards";
-import { Request } from "express";
-import { REQUEST_TOKEN_PAYLOAD_KEY } from "src/auth/auth.constants";
-import { TokenPayloadParam } from "src/auth/params/token-payload.param";
-import { TokenPayloadDto } from "src/auth/dto/token-payload.dto";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { PessoasService } from './pessoas.service';
+import { CreatePessoaDto } from './dto/create-pessoas.dto';
+import { UpdatePessoaDto } from './dto/update-pessoas.dto';
+import { AuthTokenGuard } from 'src/auth/guards/auth-token.guards';
+import { Request } from 'express';
+import { REQUEST_TOKEN_PAYLOAD_KEY } from 'src/auth/auth.constants';
+import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
+import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path'; // gera os caminhos via string
+import * as fs from 'fs/promises'; // para salvar os arquivos
 @Controller('pessoas')
 export class PessoasController {
-    constructor(private readonly pessoasService: PessoasService) {}
+  constructor(private readonly pessoasService: PessoasService) {}
+
+  @Post()
+  create(@Body() createPessoaDto: CreatePessoaDto) {
+    return this.pessoasService.create(createPessoaDto);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @Get()
+  findAll(@Req() req: Request) {
+    return this.pessoasService.findAll();
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.pessoasService.findOne(id);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updatePessoaDto: UpdatePessoaDto,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto,
+  ) {
+    return this.pessoasService.update(id, updatePessoaDto, tokenPayload);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @Delete(':id')
+  remove(
+    @Param('id', ParseIntPipe) id: number,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto,
+  ) {
+    return this.pessoasService.remove(id, tokenPayload);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  async uploadPicture(
+    @UploadedFile() file: Express.Multer.File,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto,
+  ) {
+    const fileExtension = path
+      .extname(file.originalname)
+      .toLocaleLowerCase()
+      .substring(1);
+    const fileName = `${tokenPayload.sub}.${fileExtension}`; // dessa forma a agente sobrescreve a foto antiga, pois estamos salvando-a com o mesmo nome
+    // se não fosse a intenção sobrescrever, poderíamos usar o date.now() ou o uuid para gerar um nome único
+    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName); // process.cwd() pega o diretório raiz do projeto
     
-    @Post()
-    create(@Body() createPessoaDto: CreatePessoaDto) {
-        return this.pessoasService.create(createPessoaDto);
-    }
+    await fs.writeFile(fileFullPath, file.buffer); // escreve o arquivo no disco
 
-    @UseGuards(AuthTokenGuard)
-    @Get()
-    findAll(@Req() req: Request) {
-        return this.pessoasService.findAll();
-    }
-
-    @UseGuards(AuthTokenGuard)
-    @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number) {
-        return this.pessoasService.findOne(id);
-    }
-
-    @UseGuards(AuthTokenGuard)
-    @Patch(':id')
-    update(
-        @Param('id', ParseIntPipe) id: number, 
-        @Body() updatePessoaDto: UpdatePessoaDto, 
-        @TokenPayloadParam() tokenPayload: TokenPayloadDto
-    ) {
-        return this.pessoasService.update(id, updatePessoaDto, tokenPayload);
-    }
-
-    @UseGuards(AuthTokenGuard)
-    @Delete(':id')
-    remove(
-        @Param('id', ParseIntPipe) id: number,
-        @TokenPayloadParam() tokenPayload: TokenPayloadDto
-    ) {
-        return this.pessoasService.remove(id, tokenPayload);
-    }
+    return {
+      fieldname: file.fieldname,
+      originalname: file.originalname,
+      mimetype: file.mimetype,
+      buffer: {},
+      size: file.size,
+    };
+  }
 }
